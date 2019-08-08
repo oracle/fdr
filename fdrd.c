2a84cb850f6dd262e4770a72538c480d3474b287
Bill Baker <bill.baker@oracle.com>
2019-08-08 16:47:19 -0500

    Update copyright for fdrd.c, add GPLv2 to LICENSE.txt in preparation for publishing as an external source repository.

--- fdrd.c
+++ fdrd.c
@@ -1,17 +1,10 @@
 /*
  * fdrd.c - Flight Data Recorder Daemon
  *
- * Bill Baker <Bill.Baker@oracle.com>
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Licensed under the GNU General Public License, version 2.
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
  */
 
 #include	<stdio.h>
